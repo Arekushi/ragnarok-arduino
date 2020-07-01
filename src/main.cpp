1481ba8d80d7705775b7a31bb50352eea251164b
Alexandre <alex.ferreiradelima64@gmail.com>
2020-07-01 06:33:19 -0300

    :tada: Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <Ultrasonic.h>
+
+#pragma region Variables
+
+Ultrasonic ultrasonic(7, 6);
+long microsec = 0;
+float distanceCM = 0;
+
+#pragma endregion
+
+#pragma region Methods
+
+void readDistance() {
+    distanceCM = ultrasonic.read();
+}
+
+#pragma endregion
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    readDistance();
+
+    Serial.println(distanceCM);
+}
