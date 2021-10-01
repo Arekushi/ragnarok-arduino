394bf1751238d9a7cf74a1b7d029c4b795c17866
Alexandre Ferreira de Lima <alex.ferreiradelima64@gmail.com>
2021-10-01 10:34:16 -0300

    :hammer: Create CarMain.h

---
+++ src/CarMain.h
@@ -0,0 +1,46 @@
+#ifndef car_main_h
+#define car_main_h
+
+#include <Car.h>
+
+#include <Forward.h>
+#include <Backward.h>
+#include <AligningLeft.h>
+#include <AligningRight.h>
+#include <CurvedFrontLeft.h>
+#include <CurvedFrontRight.h>
+
+#include <RXCallbacksCar.h>
+
+Car *car;
+
+void setupStates() {
+    State<Car> *forward = Singleton<Forward>::getInstance();
+    State<Car> *backward = Singleton<Backward>::getInstance();
+
+    State<Car> *aligningLeft = Singleton<AligningLeft>::getInstance();
+    State<Car> *aligningRight = Singleton<AligningRight>::getInstance();
+    
+    State<Car> *curvedFrontLeft = Singleton<CurvedFrontLeft>::getInstance();
+    State<Car> *curvedFrontRight = Singleton<CurvedFrontRight>::getInstance();
+
+    forward->opposite = backward;
+    backward->opposite = forward;
+
+    aligningLeft->opposite = aligningRight;
+    aligningRight->opposite = aligningLeft;
+
+    curvedFrontLeft->opposite = curvedFrontRight;
+    curvedFrontRight->opposite = curvedFrontLeft;
+}
+
+void setupCar() {
+    car = new Car(Singleton<Forward>::getInstance());
+    // car->startBluetooth(Singleton<RXCallbacksCar>::getInstance());
+}
+
+void executeMachine() {
+    car->machine->executeMachine();
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,13 @@
 #include <Arduino.h>
-#include <Car.h>
-#include <RXCallbacksCar.h>
-
-#include <Forward.h>
-#include <Backward.h>
-#include <AligningLeft.h>
-#include <AligningRight.h>
-#include <CurvedFrontLeft.h>
-#include <CurvedFrontRight.h>
-
-Car *car;
-
-void setupStates() {
-    State<Car> *forward = Singleton<Forward>::getInstance();
-    State<Car> *backward = Singleton<Backward>::getInstance();
-
-    State<Car> *aligningLeft = Singleton<AligningLeft>::getInstance();
-    State<Car> *aligningRight = Singleton<AligningRight>::getInstance();
-    
-    State<Car> *curvedFrontLeft = Singleton<CurvedFrontLeft>::getInstance();
-    State<Car> *curvedFrontRight = Singleton<CurvedFrontRight>::getInstance();
-
-    forward->opposite = backward;
-    backward->opposite = forward;
-
-    aligningLeft->opposite = aligningRight;
-    aligningRight->opposite = aligningLeft;
-
-    curvedFrontLeft->opposite = curvedFrontRight;
-    curvedFrontRight->opposite = curvedFrontLeft;
-}
+#include <CarMain.h>
 
 void setup() {
-    setupStates();
     Serial.begin(115200);
 
-    car = new Car(Singleton<Forward>::getInstance());
-    // car->startBluetooth(Singleton<RXCallbacksCar>::getInstance());
+    setupStates();
+    setupCar();
 }
 
 void loop() {
-    car->machine->executeMachine();
+    executeMachine();
 }
