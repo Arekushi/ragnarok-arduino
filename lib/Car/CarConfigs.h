43d7ccb7db95f7e8b84b15146cb28aabbb73af04
Alexandre Ferreira de Lima <alex.ferreiradelima64@gmail.com>
2021-10-07 10:23:37 -0300

    🎨 Move Compares to OtherConfigs

--- lib/Car/CarConfigs.h
+++ lib/Car/CarConfigs.h
@@ -6,12 +6,12 @@
 namespace PortConfigs {
     const byte ULTRA_PORTS[2] = { 16, 17 };
     const byte INFRAS_PORTS[3] = { 4, 2, 15 };
-    const unsigned short int INFRAS_COMPARERS[3] = { 1000, 400, 1000 };
     const byte PWM_CHANNELS[2] = { 0, 1 };
     const byte ENGINES_PORTS[2][3] = {{ 13, 12, 14 }, { 27, 26, 25 }};
 }
 
 namespace OtherConfigs {
+    const unsigned short int INFRAS_COMPARERS[3] = { 1000, 400, 1000 };
     const char *const INFRA_NAMES[3] = { "LEFT", "CENTER", "RIGHT" };
     const unsigned short int DELAY_FRONT_CURVE = 300;
     const byte NORMAL_SPEED = 25;
